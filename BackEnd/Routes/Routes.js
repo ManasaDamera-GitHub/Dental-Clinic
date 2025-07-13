9110f9cd06a648aaa5ed9a563a6499fe15965db7
DameraManasa-GitHub <dameramanasa25@gmail.com>
2025-07-13 15:58:21 +0530

    backend

--- BackEnd/Routes/Routes.js
+++ BackEnd/Routes/Routes.js
@@ -2,6 +2,16 @@ const express = require("express");
 const router = express.Router();
 const User = require("../Models/UserSchema.js");
 
+router.get("/", async (req, res) => {
+  try {
+    const data = await User.find();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching users" });
+    console.error("Error fetching users:", error);
+  }
+});
+
 router.post("/data", async (req, res) => {
   try {
     const { name, email, subject, message } = req.body;
